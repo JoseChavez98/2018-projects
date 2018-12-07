bef7f199b8333ca57fce0a08199e3f8f65d8683f
JoseChavez98 <jose.chavez@utec.edu.pe>
2018-12-07 00:03:24 -0500

    uploading folders

---
+++ punterosdobles/cmake-build-debug/CMakeFiles/punterosdobles.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/punterosdobles.dir/main.cpp.o"
+  "punterosdobles.pdb"
+  "punterosdobles"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/punterosdobles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
